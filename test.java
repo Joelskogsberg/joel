c6aee183c5f91d94231049d4c9c53e855dd86cb8
Joel Skogsberg <joelskogsberg@gmail.com>
2017-11-07 16:45:22 +0100

    test 4

--- test.java
+++ test.java
@@ -1,6 +1,17 @@
 ﻿
+
+public static void printWithTag(String tag, String message){
+
+        System.out.println("["+ tag +"] " + message);
+
+}
+
+
 public class PrintingClass{
  
+
+
+
        public static void main(String[] args){
 
                 System.out.println("test av andring");
