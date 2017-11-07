c07afa60c24b2b6b1d892518fe36278993b43a4a
Joel Skogsberg <joelskogsberg@gmail.com>
2017-11-07 16:27:47 +0100

    fisrt try

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class PrintingClass{
  
        public static void main(String[] args){
 
-                System.out.println("This is the initial commit");
+                System.out.println("test av andring");
 
         }
 
